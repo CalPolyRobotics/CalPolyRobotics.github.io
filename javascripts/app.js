9635a7c606ef8299b3b35cb413bfd179da43f0db
heatsink <seanswally@gmail.com>
2017-07-23 11:36:57 -0700

    kwuerch/US2-extras_sidebar

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,25 @@
-(function() {
-  'use strict';
+angular.module('firstApplication', ['ngMaterial'])
+         .controller('pageController', pageController )
+         .config(['$mdThemingProvider', function($mdThemingProvider){
+            $mdThemingProvider.theme('pageTheme')
+               .primaryPalette('blue')
+               .accentPalette('cyan')
+               .warnPalette('red');
+            $mdThemingProvider.setDefaultTheme('pageTheme');
+         }]);
 
-  angular.module('navBarDemoBasicUsage', ['ngMaterial'])
-      .controller('AppCtrl', AppCtrl);
+      function pageController( $scope, $mdSidenav, $http){
+         $scope.loadLists = function(){
+            $http.get('docs/projects.json').then(
+               function(response){
+                  $scope.projectList = response.data.projects;
+               },
+               function(response){
+                  window.alert( "Something Went Wrong :(");
+               });
+         };
 
-  function AppCtrl($scope) {
-    $scope.currentNavItem = 'page1';
-  }
-})();
+         $scope.openLeftMenu = function(){
+            $mdSidenav('left').toggle();
+         };
+      }
