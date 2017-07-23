457c5d9ac323f3523bfe8059c07da8a8b816dad5
heatsink <seanswally@gmail.com>
2017-07-23 14:15:31 -0700

    US2-extras_sidebar: Basic routing for main (non-project) pages

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,60 @@
-angular.module('firstApplication', ['ngMaterial'])
-         .controller('pageController', pageController )
-         .config(['$mdThemingProvider', function($mdThemingProvider){
-            $mdThemingProvider.theme('pageTheme')
-               .primaryPalette('blue')
-               .accentPalette('cyan')
-               .warnPalette('red');
-            $mdThemingProvider.setDefaultTheme('pageTheme');
-         }]);
+var app = angular.module('firstApplication', ['ngMaterial', 'ngRoute']);
+app.controller('pageController', pageController);
 
-      function pageController( $scope, $mdSidenav, $http){
-         $scope.loadLists = function(){
-            $http.get('docs/projects.json').then(
-               function(response){
-                  $scope.projectList = response.data.projects;
-               },
-               function(response){
-                  window.alert( "Something Went Wrong :(");
-               });
-         };
+function pageController( $scope, $mdSidenav, $http){
+   $scope.loadLists = function(){
+      $http.get('docs/projects.json').then(
+         function(response){
+            $scope.projectList = response.data.projects;
+         },
+         function(response){
+            window.alert( "Something Went Wrong :(");
+         });
+   };
 
-         $scope.openLeftMenu = function(){
-            $mdSidenav('left').toggle();
-         };
-      }
+   $scope.openLeftMenu = function(){
+      $mdSidenav('left').toggle();
+   };
+}
+
+app.config(['$mdThemingProvider', function($mdThemingProvider){
+   $mdThemingProvider.theme('pageTheme')
+      .primaryPalette('blue')
+      .accentPalette('cyan')
+      .warnPalette('red');
+   $mdThemingProvider.setDefaultTheme('pageTheme');
+}]);
+
+// Route Configuration
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "pages/main.htm",
+        })
+        .when("/store", {
+            templateUrl : "pages/store.htm",
+            controller : "storeCtrl"
+        })
+        .when("/contact", {
+            templateUrl : "pages/contact.htm",
+            controller : "contactCtrl"
+        })
+        .when("/about", {
+            templateUrl : "pages/about.htm",
+            controller : "aboutCtrl"
+        });
+});
+
+// Router Controller functions
+// NOTE: not every page will need a helper controller. Email List landing page will probably need one though
+app.controller("storeCtrl", function ($scope) {
+    $scope.msg = "Best Store Ever";
+});
+
+app.controller("contactCtrl", function ($scope) {
+    $scope.msg = "Best Contact Ever";
+});
+
+app.controller("aboutCtrl", function ($scope) {
+    $scope.msg = "Best Contact Ever";
+});
