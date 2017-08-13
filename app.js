310d7ab8beaa72f80cddfd9e4380452b7e1a283e
heatsink <seanswally@gmail.com>
2017-08-12 18:25:11 -0700

    Testing github pages functionality

---
+++ app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', [
+  'ngRoute',
+  'ngMaterial'
+]);
+
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/main'});
+}]);
+
+app.controller('pageController', pageController);
+function pageController( $scope, $mdSidenav, $http, $location){
+    $scope.getClass = function(path){
+        if($location.path().substr(0, path.length) == path ){
+            return "background-active";
+        }else{
+            return "";
+        }
+    };
+
+    $scope.loadLists = function(){
+        $http.get('pages/projects/projects.json').then(
+            function(response){
+                $scope.projectList = response.data.projects;
+            },
+            function(response){
+                window.alert( "Something Went Wrong :(");
+            });
+    };
+
+    $scope.toggleLeftMenu = function(){
+        $mdSidenav('left').toggle();
+        $scope.showProjectList = false;
+    };
+}
+
+// Route Configuration
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("", {
+            templateUrl : "pages/main.htm",
+        })
+        .when("/main", {
+            templateUrl : "pages/main.htm",
+        })
+        .when("/projects", {
+            templateUrl : "pages/projects.htm"
+        })
+        .when("/store", {
+            templateUrl : "pages/store.htm",
+            controller : "storeCtrl"
+        })
+        .when("/contact", {
+            templateUrl : "pages/contact.htm",
+            controller : "contactCtrl"
+        })
+        .when("/about", {
+            templateUrl : "pages/about.htm",
+            controller : "aboutCtrl"
+        })
+        .when("/mailing", {
+            templateUrl : "pages/mailing.htm",
+        })
+        .when("/projects/:name",{
+            templateUrl : function($routeParams) {
+                return "pages/projects/" + $routeParams.name;
+            }
+        });
+
+});
